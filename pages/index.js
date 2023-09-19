7f70077b8ace04c9530e5c704576a5f16939c6e3
Oumnya benhassou <oumnya@users.noreply.github.com>
2023-09-20 00:48:21 +0400

    update link

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://cloud-station.io"
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
@@ -53,12 +53,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
+
       </footer>
     </div>
   )
